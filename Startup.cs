de3cf2cc9003ad325bc77fb44cd137bfa3d8f877
Kevin Logan <klogan@omnitech-inc.com>
2018-08-19 21:33:08 -0500

    .hintrc and added UseCsp, UseXfo

--- Startup.cs
+++ Startup.cs
@@ -72,6 +72,17 @@ namespace webhintMvcCoreArticle
             app.UseXContentTypeOptions();
             app.UseReferrerPolicy(opts => opts.NoReferrer());
             app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            app.UseXfo(options => options.Deny());
+            app.UseCsp(opts => opts
+            .BlockAllMixedContent()
+            .StyleSources(s => s.Self())
+            .StyleSources(s => s.UnsafeInline())
+            .FontSources(s => s.Self())
+            .FormActions(s => s.Self())
+            .FrameAncestors(s => s.Self())
+            .ImageSources(s => s.Self())
+            .ScriptSources(s => s.Self())
+        );
         }
     }
 }
